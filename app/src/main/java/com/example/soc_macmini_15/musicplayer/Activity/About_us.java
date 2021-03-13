f45c05fd1106b502be029dcdb462874f8ab7f3c4
Sohan Birajdar <66657628+Sohan710@users.noreply.github.com>
2021-03-13 13:29:46 +0530

    Project

---
+++ app/src/main/java/com/example/soc_macmini_15/musicplayer/Activity/About_us.java
@@ -0,0 +1,13 @@
+package com.example.soc_macmini_15.musicplayer.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class About_us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
